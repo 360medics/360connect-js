46c83cc4442cba5c537361c67df9cad7816bfbea
adadgio <romain.bruckert.esc@gmail.com>
2021-01-22 11:40:16 +0100

    First commit.

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+var Connect = /** @class */ (function () {
+    function Connect(baseUrl, clientKey) {
+        this.$baseUrl = baseUrl;
+        this.$clientKey = clientKey;
+        this.$loginUrl = this.$baseUrl + "/connect/autorization_form?clientKey=" + this.$clientKey;
+    }
+    Connect.prototype.getLoginUrl = function () {
+        if (null !== this.getParam('api_key')) {
+            return this.$loginUrl + '&api_key=' + this.getParam('api_key');
+        }
+        return this.$loginUrl;
+    };
+    Connect.prototype.userAuthorizationValid = function () {
+        this.$token = this.getParam('token');
+        return (this.$token !== null);
+    };
+    Connect.prototype.getToken = function () {
+        return this.$token;
+    };
+    Connect.prototype.getUser = function () {
+        var url = this.$baseUrl + "/connect/user?clientKey=" + this.$clientKey + "&token=" + this.$token;
+        return new Promise(function (resolve, reject) {
+            axios.get(url)
+                .then(function (r) {
+                resolve(r.data);
+            }).catch(function (e) {
+                reject(e);
+            });
+        });
+    };
+    Connect.prototype.getParam = function (name) {
+        var urlParams = new URLSearchParams(window.location.search);
+        return urlParams.get(name);
+    };
+    return Connect;
+}());
---
+++ src/index.ts
@@ -0,0 +1,51 @@
+declare var axios: any
+
+class Connect
+{
+    $baseUrl: string
+    $clientKey: string
+    $loginUrl: string
+    $token: string
+    $user: any
+    
+    constructor(baseUrl: string, clientKey: string) {
+        this.$baseUrl = baseUrl
+        this.$clientKey = clientKey
+        this.$loginUrl = `${this.$baseUrl}/connect/autorization_form?clientKey=${this.$clientKey}`
+    }
+    
+    getLoginUrl() {
+        if (null !== this.getParam('api_key')) {
+            return this.$loginUrl + '&api_key=' + this.getParam('api_key')
+        }
+        
+        return this.$loginUrl
+    }
+
+    userAuthorizationValid() {
+        this.$token = this.getParam('token')
+        return (this.$token !== null)
+    }
+
+    getToken() {
+        return this.$token
+    }
+    
+    getUser() {
+        const url = `${this.$baseUrl}/connect/user?clientKey=${this.$clientKey}&token=${this.$token}`
+
+        return new Promise((resolve, reject) => {
+            axios.get(url)
+                .then(r => {
+                    resolve(r.data)
+                }).catch(e => {
+                    reject(e)
+                })
+        })
+    }
+
+    getParam(name: string) {
+        const urlParams = new URLSearchParams(window.location.search)
+        return urlParams.get(name)
+    }
+}
